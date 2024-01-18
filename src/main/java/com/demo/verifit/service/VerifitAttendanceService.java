6ef8e984587b4bbe22affe39aa55040be4b73147
batwoman30 <156612676+batwoman30@users.noreply.github.com>
2024-01-18 22:49:19 +0000

    Update VerifitAttendanceService.java isDiscountEligible logic

--- src/main/java/com/demo/verifit/service/VerifitAttendanceService.java
+++ src/main/java/com/demo/verifit/service/VerifitAttendanceService.java
@@ -5,12 +5,13 @@ import com.demo.verifit.repository.VerifitAttendanceRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.WeekFields;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Service
 public class VerifitAttendanceService {
     @Autowired
@@ -34,17 +35,32 @@ public class VerifitAttendanceService {
         }
 
         // Calculate the start date for the comparison (three weeks ago from the latest attendance)
-        LocalDate startDate = verifitAttendanceRecords.get(0).getAttendanceDate().toLocalDate().minusWeeks(3);
-
-        // Count the number of attendances in the last three weeks
-        long weeklyAttendances = verifitAttendanceRecords.stream()
-                .filter(record -> record.getAttendanceDate().toLocalDate().isAfter(startDate))
-                .count();
+        LocalDate startDate = verifitAttendanceRecords.get(0).getAttendanceDate().toLocalDate();
 
         // Calculate the expected number of attendances (once a week for three weeks)
         long expectedAttendances = 3;
 
-        return weeklyAttendances >= expectedAttendances;
+        // Count the number of attendances in the last three weeks
+        Map<Integer, VerifitAttendanceRecord> weekToAttendance = verifitAttendanceRecords.stream()
+                .collect(Collectors.toMap(attendanceRecord -> getWeekNumber(startDate, attendanceRecord.getAttendanceDate().toLocalDate()),
+                        attendanceRecord -> attendanceRecord,
+                        (r1, r2) -> r1));
+        weekToAttendance.remove(0);
+        return weekToAttendance.size() >= expectedAttendances;
+    }
+
+
+    public static int getWeekNumber(LocalDate startDate, LocalDate currentDate) {
+        if(currentDate.isAfter(startDate.minusWeeks(1))) {
+            return 3;
+        } else if(currentDate.isBefore(startDate.minusWeeks(1)) &&
+                currentDate.isAfter(startDate.minusWeeks(2))) {
+            return 2;
+        } else if(currentDate.isBefore(startDate.minusWeeks(2)) &&
+                currentDate.isAfter(startDate.minusWeeks(3))) {
+            return 1;
+        }
+        return 0;
     }
 
     public int getUserCurrentStreak(String userId) {
@@ -56,13 +72,13 @@ public class VerifitAttendanceService {
         }
         int streak = 1;
         int weekNumber = 0;
+
         // Calculate the start date for the comparison
-
         for(int i = 0; i < verifitAttendanceRecords.size(); i++) {
-
             VerifitAttendanceRecord currentRecord = verifitAttendanceRecords.get(i);
             LocalDate currentDate = currentRecord.getAttendanceDate().toLocalDate();
-            int currentDatesWeek = currentDate.get(WeekFields.ISO.weekOfWeekBasedYear());;
+
+            int currentDatesWeek = currentDate.get(WeekFields.ISO.weekOfWeekBasedYear());
             if(weekNumber == 0) {
                 weekNumber = currentDatesWeek;
             } else if(weekNumber == currentDatesWeek){
